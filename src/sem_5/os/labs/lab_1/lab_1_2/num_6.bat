b7abca0552f6bccecd1fa47090586985c8c52823
maxim1770 <maxim1770@gmail.com>
2022-09-22 00:35:47 +0300

    refactor(os): поправить for в лабе 1.2 num_6
    Объединить два for в один.
    Сделать так в двух местах.

--- src/sem_5/os/labs/lab_1/lab_1_2/num_6.bat
+++ src/sem_5/os/labs/lab_1/lab_1_2/num_6.bat
@@ -9,9 +9,6 @@ set /a number_hidden_subdirectories = 0
 
 for /f "tokens=*" %%i in ('dir /a:DH /b %dir_path%') do (
     echo %dir_path%\%%~i
-)
-
-for /f "tokens=*" %%i in ('dir /a:DH /b %dir_path%') do (
     set /a number_hidden_subdirectories = number_hidden_subdirectories + 1
 )
 
@@ -25,12 +22,7 @@ set /a number_text_files = 0
 
 for /f "tokens=*" %%i in ('dir /b %dir_path%\*.txt') do (
     echo %dir_path%\%%~i
-)
-
-for /f "tokens=*" %%i in ('dir /b %dir_path%\*.txt') do (
     set /a number_text_files = number_text_files + 1
 )
 
 echo Number of text files = %number_text_files%
-
-
