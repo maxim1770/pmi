ba30aa21dd49ff64d3060e78d1ec458cfc4fef3f
maxim1770 <maxim1770@gmail.com>
2022-09-21 23:21:51 +0300

    feat(os): доделать лабу 1.2 num_6

--- src/sem_5/os/labs/lab_1/lab_1_2/num_6.bat
+++ src/sem_5/os/labs/lab_1/lab_1_2/num_6.bat
@@ -6,58 +6,31 @@
 set dir_path=%1
 
 set /a number_hidden_subdirectories = 0
-for /r %dir_path% %%i in (.*) do echo %%~fi
-for /r %dir_path% %%i in (.*) do set /a number_hidden_subdirectories = number_hidden_subdirectories + 1
 
-echo Number of hidden subdirectories = %number_hidden_subdirectories%
+for /f "tokens=*" %%i in ('dir /a:DH /b %dir_path%') do (
+    echo %dir_path%\%%~i
+)
 
-
-for /f "tokens=*" %%i in ('dir /a:DH /b C:\Users\MaxDroN\python_projects\python_work\pmi') do echo %%~i
-
-set /a number_hidden_subdirectories = 0
-for /f "tokens=*" %%i in ('dir /a:DH /b C:\Users\MaxDroN\python_projects\python_work\pmi') do (
+for /f "tokens=*" %%i in ('dir /a:DH /b %dir_path%') do (
     set /a number_hidden_subdirectories = number_hidden_subdirectories + 1
 )
-echo Number of hidden subdirectories = %number_hidden_subdirectories%
 
+echo Number of hidden subdirectories = %number_hidden_subdirectories%
 
 
 :: Пункт 2
 echo -------------------------------
 
 set /a number_text_files = 0
-for /r %dir_path% %%i in (*.txt) do (
-    echo %%~fi
+
+for /f "tokens=*" %%i in ('dir /b %dir_path%\*.txt') do (
+    echo %dir_path%\%%~i
 )
 
-for /r %dir_path% %%i in (*.txt) do (
+for /f "tokens=*" %%i in ('dir /b %dir_path%\*.txt') do (
     set /a number_text_files = number_text_files + 1
 )
 
 echo Number of text files = %number_text_files%
 
-
-echo -------------------------------
-
-::for /r %%i in ('dir /A:DH /B "%dir_path%"') do (
-::    echo %%~fi
-::)
-
-
-
-for /r %dir_path% %%i in () do echo %%~fi
-
-echo -------------------------------
-
-
-
-set t=dir %dir_path% /a:DH /B
-::echo %t%
-
-
-echo -------------------------------
-
-for %dir_path% %%i in (*) do echo %%~i
-
-echo -------------------------------
 
